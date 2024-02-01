2ad8fcb6463bfd32514a62ceaf3c815e32301888
shashank-chutke <shashank@pensieve.id>
2024-02-01 19:55:16 +0530

    added sample user accounts and fixed sign up bug

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,86 +1,79 @@
-import { useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import styles from "./styles.module.css";
-import Spinner from "react-spinkit";
+import {useState} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+import styles from './styles.module.css';
+import Spinner from 'react-spinkit';
 
 const Login = () => {
-  const [data, setData] = useState({ email: "", password: "" });
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({email: '', password: ''});
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
-  const handleChange = ({ currentTarget: input }) => {
-    setData({ ...data, [input.name]: input.value });
-  };
+    const handleChange = ({currentTarget: input}) => {
+        setData({...data, [input.name]: input.value});
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      setLoading(true);
-      const { data: res } = await axios.post("/auth", data);
-      localStorage.setItem("token", res.data);
-      window.location = "/";
-      setLoading(false);
-    } catch (error) {
-      if (
-        error.response &&
-        error.response.status >= 400 &&
-        error.response.status <= 500
-      ) {
-        setError(error.response.data.message);
-      }
-      setLoading(false);
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const {data: res} = await axios.post('/auth', data);
+            localStorage.setItem('token', res.data);
+            window.location = '/';
+            setLoading(false);
+        } catch (error) {
+            if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+                setError(error.response.data.message);
+            }
+            setLoading(false);
+        }
+    };
 
-  return (
-    <div className={styles.login_container}>
-      <div className={styles.login_form_container}>
-        <div className={styles.left}>
-          <form className={styles.form_container} onSubmit={handleSubmit}>
-            <h1>Login to Your Account</h1>
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              onChange={handleChange}
-              value={data.email}
-              required
-              className={styles.input}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-              value={data.password}
-              required
-              className={styles.input}
-            />
-            {error && <div className={styles.error_msg}>{error}</div>}
-            <button type="submit" className={styles.green_btn}>
-              Sign In
-            </button>
-            {loading && (
-              <Spinner
-                name="circle"
-                className={styles.login_spinner}
-                style={{ width: 50, height: 50 }}
-              />
-            )}
-          </form>
+    return (
+        <div className={styles.login_container}>
+            <div className={styles.login_form_container}>
+                <div className={styles.left}>
+                    Email: testing1234@test.com <br></br> Password: !234Qwer
+                    <form className={styles.form_container} onSubmit={handleSubmit}>
+                        <h1>Login to Your Account</h1>
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            onChange={handleChange}
+                            value={data.email}
+                            required
+                            className={styles.input}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            onChange={handleChange}
+                            value={data.password}
+                            required
+                            className={styles.input}
+                        />
+                        {error && <div className={styles.error_msg}>{error}</div>}
+                        <button type="submit" className={styles.green_btn}>
+                            Sign In
+                        </button>
+                        {loading && (
+                            <Spinner name="circle" className={styles.login_spinner} style={{width: 50, height: 50}} />
+                        )}
+                    </form>
+                </div>
+                <div className={styles.right}>
+                    <h1>New Here ?</h1>
+                    <Link to="/signup">
+                        <button type="button" className={styles.white_btn}>
+                            Sign Up
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
-        <div className={styles.right}>
-          <h1>New Here ?</h1>
-          <Link to="/signup">
-            <button type="button" className={styles.white_btn}>
-              Sign Up
-            </button>
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
